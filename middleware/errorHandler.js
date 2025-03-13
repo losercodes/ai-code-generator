58b06e310adb3e3102647566e2844b2a411c75e1
losercodes <aman0hata@gmail.com>
2025-03-13 10:46:53 +0530

    meow

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -9,12 +9,10 @@ const errorHandler = (err, req, res, next) => {
       stack: process.env.NODE_ENV === 'production' ? '🥞' : err.stack
     });
   };
-  
   const notFound = (req, res, next) => {
-    const error = new Error(`Not Found - ${req.originalUrl}`);
-    res.status(404);
-    next(error);
-  };
+    res.status(404).json({ message: `Not Found - ${req.originalUrl}` });
+};
+
   
   module.exports = { errorHandler, notFound };
   
--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// File: server.js
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
